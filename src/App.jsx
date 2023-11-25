80e79fafe6657e442508545e456c17bbce572905
Dany <dany21sayan_@hotmail.com>
2023-11-25 15:16:18 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import HomePage from './components/pages/HomePage'
+
+function App() {
+
+
+  return (
+    <div className='main__wrapper'>
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
